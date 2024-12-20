940c995d440af9b49e620461fabbb579f04ae05c
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-12-20 22:38:51 +0530

    Landing page - hero complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className='HEADER'>
+			<div className='navbar glass'>
+				<Link to='/home'>Home</Link>
+				<Link to='/events'>Events</Link>
+				<Link to='/fest-access'>Fest Access</Link>
+				<Link to='/schedule'>Schedule</Link>
+				<Link to='/sponsors'>Sponsors</Link>
+				<Link to='/contact-us'>Contact Us</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Header from '../components/Header';
 
 const Home = () => {
-	return <div>Home</div>;
+	useEffect(() => {
+		document.title = 'Verba Maximus - Home';
+	}, []);
+
+	return (
+		<div className='HOME'>
+			<img src='/assets/hero.png' />
+			<Header />
+			<div className='hero'>
+				<h1>Verba Maximus</h1>
+				<h2>echoes of eden</h2>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
