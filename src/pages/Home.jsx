ea75e96bb687ace3fe50f40827c1582622e9b379
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-12-23 18:11:43 +0530

    Added introduction section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,24 @@ const Home = () => {
 				<h1>Verba Maximus</h1>
 				<h2>echoes of eden</h2>
 			</div>
+			<div className='introduction'>
+				<h1>Echoes of Eden</h1>
+				<p>
+					Verba Maximus is BITS Pilani, Hyderabad Campus’ annual
+					literary fest, organized by the passionate minds of the
+					English Language Activities Society (ELAS). Since its
+					inception in 2012, the fest has brought together a myriad of
+					events, competitions, and performances — pitting the
+					sharpest literary minds against each other.
+				</p>
+				<p>
+					With captivating acts from renowned artists and an
+					atmosphere brimming with creativity and excitement, Verba
+					Maximus promises an unforgettable experience. This January,
+					we return with our thirteenth edition and invite you to step
+					into our world and witness the magic unfold.
+				</p>
+			</div>
 		</div>
 	);
 };
