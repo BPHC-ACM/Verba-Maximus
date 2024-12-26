7380e7d288504083b21cdec484c300f805f92b03
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-12-26 11:13:24 +0530

    Fix homepage styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,9 +14,6 @@ const Header = () => {
 			<div className='navbar'>
 				{isMobile ? (
 					<>
-						<div className='register-container glass'>
-							<Link to='/register'>Register</Link>
-						</div>
 						<div
 							className='hamburger glass'
 							onClick={toggleMobileMenu}
@@ -35,19 +32,17 @@ const Header = () => {
 						)}
 					</>
 				) : (
-					<>
+					<div className='navbar glass'>
 						<Link to='/home'>Home</Link>
 						<Link to='/events'>Events</Link>
 						<Link to='/fest-access'>Fest Access</Link>
 						<Link to='/schedule'>Schedule</Link>
-					</>
+					</div>
 				)}
-			</div>
-			{!isMobile && (
 				<div className='register-container glass'>
 					<Link to='/register'>Register</Link>
 				</div>
-			)}
+			</div>
 		</div>
 	);
 };
