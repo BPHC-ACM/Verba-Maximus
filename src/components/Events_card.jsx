02931559b337d7cf377f8595fd3600ba46cfbaad
DevanshAgarwal1806 <devansh180606@gmail.com>
2024-12-27 17:21:46 +0530

    added search and filter options in events page

---
+++ src/components/Events_card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/components/_eventscard.scss'
+const Events_card = (props) => {
+  return (
+    <div className="Event_card glass">fjdfjlk</div>
+  )
+}
+
+export default Events_card
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
+import Events_card from '../components/Events_card';
 const Events = () => {
 	useEffect(() => {
 		document.title = 'Verba Maximus - Events';
@@ -11,6 +12,32 @@ const Events = () => {
 			<Header />
 			<div className='heading'>
 				<h1>Events</h1>
+			</div>
+			<div className="mainevents">
+				<div className="Searchnfilter">
+					<input className="searchinput glass"
+                    	type="text"
+                    	placeholder="Search events..."
+                    	// value={searchTerm}
+                   	 	// onChange={(e) => setSearchTerm(e.target.value)}
+              	  	/>
+					<select class="filter glass"
+                    // value={filterClub}
+                    // onChange={(e) => setFilterClub(e.target.value)}
+                	>
+                    <option value="">Filter by club</option>
+                    <option value="ELAS">ELAS</option>
+                    <option value="Hindi Tarang">Hindi Tarang</option>
+                    <option value="SAFL">SAFL</option>
+					<option value="Quiz Club">Quiz Club</option>
+					<option value="Fashion Club">Fashion Club</option>
+					<option value="Journal Club">Journal Club</option>
+					<option value="Music Club">Music Club</option>
+                </select>
+				</div>
+				<div className="eventsholder">
+					{/* <Events_card/> */}
+				</div>
 			</div>
 			<Footer/>
 		</div>
