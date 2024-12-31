2f6c70963be1181cf763105e10dff73835209c50
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-12-31 14:26:57 +0530

    Fixed warning

--- src/components/Events_card.jsx
+++ src/components/Events_card.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import {
 	Card,
 	CardContent,
@@ -19,86 +19,101 @@ const modalStyle = {
 	borderRadius: 2,
 };
 const Events_card = (props) => {
-  const [open, setOpen] = useState(false);
-  
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-  
-    return (
-      <div className='EVENTCARD'>
-        <Card
-          sx={{
-            maxWidth: 345,
-            margin: 2,
-          }}
-          className='glass'
-          onClick={handleOpen}
-        >
-          <CardContent>
-            <Typography variant='h5'>{props.name}</Typography>
-            <Typography variant='body2' color='text.secondary'>
-              {props.shtdesc}
-            </Typography>
-          </CardContent>
-        </Card>
-  
-        <Modal open={open} onClose={handleClose}>
-          <Box sx={{
-              modalStyle,
-              maxWidth:'70vw',
-              maxHeight:'80vh',
-              overflowY: 'auto',
-              position:'absolute',
-              top:'50%',
-              left:'50%',
-              transform: 'translate(-50%, -50%)',
-              p:4,
-              '&::-webkit-scrollbar': {
-                width: '8px',
-              },
-              '&::-webkit-scrollbar-thumb': {
-                backgroundColor: 'rgb(139, 117, 97)', 
-                borderRadius: '4px', 
-              },
-              bgcolor: 'background.paper',
-	            boxShadow: 24,
-	            p: 4,
-	            borderRadius: 2,
-          }} className='modal'>
-            <Typography variant='h2'>{props.name}</Typography>
-            <Box sx={{ mt: 2 }}>
-              {props.details.map((paragraph, index) => (
-                <Typography key={index} variant='body2' paragraph>
-                  {paragraph}
-                </Typography>
-              ))}
-            </Box>
-            {props.rules && props.rules.length>0 && (
-              <>
-                <Typography variant='h3' style={{ textAlign: 'center', fontSize: '30px', letterSpacing: '3px', fontWeight:100}} 
-                >Rules and Instructions</Typography>
-                <Box sx={{ mt: 2 }}>
-                {props.rules.map((paragraph, index) => (
-                <Typography key={index} variant='body2' paragraph>
-                    {paragraph}
-                </Typography>
-              ))}
-            </Box>
-              </>
-            )}
-            <Button
-              variant='outlined'
-              color='warning'
-              onClick={handleClose}
-              sx={{ marginTop: 2 }
-              } className="glass"
-            >
-              Close
-            </Button>
-          </Box>
-        </Modal>
-      </div>
-    );
-}
+	const [open, setOpen] = useState(false);
 
-export default Events_card
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	return (
+		<div className='EVENTCARD'>
+			<Card
+				sx={{
+					maxWidth: 345,
+					margin: 2,
+				}}
+				className='glass'
+				onClick={handleOpen}
+			>
+				<CardContent>
+					<Typography variant='h5'>{props.name}</Typography>
+					<Typography variant='body2' color='text.secondary'>
+						{props.shtdesc}
+					</Typography>
+				</CardContent>
+			</Card>
+
+			<Modal open={open} onClose={handleClose}>
+				<Box
+					sx={{
+						modalStyle,
+						maxWidth: '70vw',
+						maxHeight: '80vh',
+						overflowY: 'auto',
+						position: 'absolute',
+						top: '50%',
+						left: '50%',
+						transform: 'translate(-50%, -50%)',
+						p: 4,
+						'&::-webkit-scrollbar': {
+							width: '8px',
+						},
+						'&::-webkit-scrollbar-thumb': {
+							backgroundColor: 'rgb(139, 117, 97)',
+							borderRadius: '4px',
+						},
+						bgcolor: 'background.paper',
+						boxShadow: 24,
+						borderRadius: 2,
+					}}
+					className='modal'
+				>
+					<Typography variant='h2'>{props.name}</Typography>
+					<Box sx={{ mt: 2 }}>
+						{props.details.map((paragraph, index) => (
+							<Typography key={index} variant='body2' paragraph>
+								{paragraph}
+							</Typography>
+						))}
+					</Box>
+					{props.rules && props.rules.length > 0 && (
+						<>
+							<Typography
+								variant='h3'
+								style={{
+									textAlign: 'center',
+									fontSize: '30px',
+									letterSpacing: '3px',
+									fontWeight: 100,
+								}}
+							>
+								Rules and Instructions
+							</Typography>
+							<Box sx={{ mt: 2 }}>
+								{props.rules.map((paragraph, index) => (
+									<Typography
+										key={index}
+										variant='body2'
+										paragraph
+									>
+										{paragraph}
+									</Typography>
+								))}
+							</Box>
+						</>
+					)}
+					<Button
+						variant='outlined'
+						color='warning'
+						onClick={handleClose}
+						sx={{ marginTop: 2 }}
+						className='glass'
+					>
+						Close
+					</Button>
+				</Box>
+			</Modal>
+		</div>
+	);
+};
+
+export default Events_card;
